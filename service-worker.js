3c3039fc76c67876d094343001bb1501d9daa7b3
aldenarends2 <alden.arends2@gmail.com>
2026-07-06 17:49:33 +0200

    Bump cache and include Abbeydale assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,11 @@
-const CACHE_NAME = 'abbeydale-yardops-v3';
-const CORE_FILES = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'abbeydale-yardops-v6';
+const CORE_FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/abbeydale-logo.svg?v=6',
+  './assets/icon.svg?v=6'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_FILES)));
@@ -7,12 +13,22 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request).then(match => match || caches.match('./index.html')))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(match => match || caches.match('./index.html')))
   );
 });
