52718eb040e1edacfc3e235b9a30ffca872b1ac1
aldenarends2 <alden.arends2@gmail.com>
2026-07-06 17:13:49 +0200

    Improve service worker install behaviour

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,18 @@
-const CACHE_NAME = 'abbeydale-yardops-v2';
+const CACHE_NAME = 'abbeydale-yardops-v3';
+const CORE_FILES = ['./', './index.html', './manifest.json'];
+
 self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_FILES)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', event => {
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request).then(match => match || caches.match('./index.html')))
+  );
 });
