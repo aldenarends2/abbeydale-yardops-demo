79597e676dc1636a6d448080ff1f638e231232af
aldenarends2 <alden.arends2@gmail.com>
2026-07-06 22:09:02 +0200

    Reset service worker cache for mobile layout

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,18 @@
-const CACHE_NAME = 'abbeydale-yardops-v10';
-const CORE_FILES = [
-  './',
-  './index.html',
-  './manifest.json',
-  './assets/abbeydale-logo.svg?v=10',
-  './assets/icon.svg?v=10'
-];
+const CACHE_NAME = 'abbeydale-yardops-v11';
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_FILES)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request).then(match => match || caches.match('./index.html')))
-  );
+  event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
 });
