3adf11ac058018265d69cc6a3a90d1dcb56a9d06
aldenarends2 <alden.arends2@gmail.com>
2026-07-07 07:05:01 +0200

    Reset cache for Phase 3D Job Card Engine

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,6 @@
-const CACHE_NAME = 'abbeydale-yardops-v17';
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-
+const CACHE_NAME = 'abbeydale-yardops-v18';
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+  if (event.request.method === 'GET') event.respondWith(fetch(event.request));
 });
